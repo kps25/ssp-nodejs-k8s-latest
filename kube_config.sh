835304d5b83171bb1c14eea38e8cb9b80c24bf03
ssp <ssp@gmail.com>
2018-08-17 03:19:12 +0000

    New fles

--- kube_config.sh
+++ kube_config.sh
@@ -9,5 +9,5 @@ kubectl config set-cluster ${CLUSTER_NAME} --server=${API_SERVER} --insecure-ski
 kubectl config set-credentials ${CLUSTER_NAME} --token=${TOKEN}
 kubectl config set-context dev.sspcloudpro.co.in  --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=dev
 # To test that configuration works
-kubectl get pods --context="dev.sspcloudpro.co.in" --namespace dev
-kubectl config get-contexts --namespace dev
+kubectl get pods --context="k8s.sspcloudpro.co.in" --namespace develop
+kubectl config get-contexts --namespace develop
