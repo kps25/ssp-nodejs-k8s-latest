f8ad3398ef9fae499792d59a0123408286c22eed
awsdevopsssp <37306479+awsdevopsssp@users.noreply.github.com>
2018-08-17 09:21:44 +0530

    Change in path

--- jenkins/kube_config.sh
+++ jenkins/kube_config.sh
@@ -4,10 +4,10 @@ API_SERVER="https://api.dev.sspcloudpro.co.in"
 TOKEN="GYWwTaICieMPthhrNp0XXqH6qvHYC0xq"
 CLUSTER_NAME="dev.sspcloudpro.co.in"
 
-kubectl config set preferences.colors true
-kubectl config set-cluster ${CLUSTER_NAME} --server=${API_SERVER} --insecure-skip-tls-verify=true
-kubectl config set-credentials ${CLUSTER_NAME} --token=${TOKEN}
-kubectl config set-context dev.sspcloudpro.co.in  --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=dev
+/usr/local/bin/kubectl config set preferences.colors true
+/usr/local/bin/kubectl config set-cluster ${CLUSTER_NAME} --server=${API_SERVER} --insecure-skip-tls-verify=true
+/usr/local/bin/kubectl config set-credentials ${CLUSTER_NAME} --token=${TOKEN}
+/usr/local/bin/kubectl config set-context dev.sspcloudpro.co.in  --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=dev
 # To test that configuration works
-kubectl get pods --context="dev.sspcloudpro.co.in" --namespace dev
-kubectl config get-contexts --namespace dev
+/usr/local/bin/kubectl get pods --context="dev.sspcloudpro.co.in" --namespace dev
+/usr/local/bin/kubectl config get-contexts --namespace dev
