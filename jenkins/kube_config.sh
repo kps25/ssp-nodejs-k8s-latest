ec56c420988ffc97b9fa13753dcaf7067ae7bf26
awsdevopsssp <37306479+awsdevopsssp@users.noreply.github.com>
2018-11-03 22:58:47 +0530

    Update kube_config.sh

--- jenkins/kube_config.sh
+++ jenkins/kube_config.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-API_SERVER="https://api.k8s.sspcloudpro.co.in"
+API_SERVER="https://api.ragava.sspcloudpro.co.in"
 TOKEN="7IR4vOtqNKs7VnbATFUo2ZFgJ0IfW7gT"
-CLUSTER_NAME="k8s.sspcloudpro.co.in"
+CLUSTER_NAME="ragava.sspcloudpro.co.in"
 
 /usr/local/bin/kubectl config set preferences.colors true
 /usr/local/bin/kubectl config set-cluster ${CLUSTER_NAME} --server=${API_SERVER} --insecure-skip-tls-verify=true
 /usr/local/bin/kubectl config set-credentials ${CLUSTER_NAME} --token=${TOKEN}
-/usr/local/bin/kubectl config set-context k8s.sspcloudpro.co.in  --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=default
+/usr/local/bin/kubectl config set-context ragava.sspcloudpro.co.in  --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=default
 # To test that configuration works
-/usr/local/bin/kubectl get pods --context="k8s.sspcloudpro.co.in" --namespace default
+/usr/local/bin/kubectl get pods --context="ragava.sspcloudpro.co.in" --namespace default
 /usr/local/bin/kubectl config get-contexts --namespace default
