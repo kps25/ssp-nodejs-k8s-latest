0f7114d215a36c7535ff8d363e944559314f3eb7
awsdevopsssp <37306479+awsdevopsssp@users.noreply.github.com>
2019-05-23 09:05:55 +0530

    Update kube_config.sh

--- kube_config.sh
+++ kube_config.sh
@@ -9,5 +9,5 @@ kubectl config set-cluster ${CLUSTER_NAME} --server=${API_SERVER} --insecure-ski
 kubectl config set-credentials ${CLUSTER_NAME} --token=${TOKEN}
 kubectl config set-context devops.sspcloudpro.co.in  --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=default
 # To test that configuration works
-kubectl get pods --context="k8s.sspcloudpro.co.in" --namespace default
+kubectl get pods --context="devops.sspcloudpro.co.in" --namespace default
 kubectl config get-contexts --namespace default
