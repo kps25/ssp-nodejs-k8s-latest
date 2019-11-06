66d86f4eeb953108f5c030d6c525e58383b6c515
awsdevopsssp <37306479+awsdevopsssp@users.noreply.github.com>
2019-11-06 18:27:40 +0530

    Update kube_config.sh

--- kube_config.sh
+++ kube_config.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash
-
 API_SERVER="https://api.sspcloudpro.co.in"
 TOKEN="asbm9rjkHREcZED7d75oWv0U29bjdhn3"
 CLUSTER_NAME="sspcloudpro.co.in"
 
 kubectl config set preferences.colors true
-kubectl config set-cluster ${CLUSTER_NAME} --server=${API_SERVER} --insecure-skip-tls-verify=true
-kubectl config set-credentials ${CLUSTER_NAME} --token=${TOKEN}
-kubectl config set-context sspcloudpro.co.in  --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=default
-# To test that configuration works
-kubectl get pods --context="sspcloudpro.co.in" --namespace default
-kubectl config get-contexts --namespace default
+
+kubectl config set-cluster ${CLUSTER_NAME} --server=${API_SERVER} --insecure-skip-tls-verify=true --namespace default
+kubectl config set-credentials ${CLUSTER_NAME} --token=${TOKEN} --namespace default
+
+# Set context to new root cluster
+kubectl config set-context ${CLUSTER_NAME} --namespace default
+
+# Print all contexts
+kubectl config get-contexts
+kubectl get ns
