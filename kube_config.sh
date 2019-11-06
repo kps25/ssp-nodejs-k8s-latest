41350343f23448a483d240b021342805eadcf8ea
awsdevopsssp <37306479+awsdevopsssp@users.noreply.github.com>
2019-11-06 13:32:18 +0530

    Update kube_config.sh

--- kube_config.sh
+++ kube_config.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-API_SERVER="https://api.devops.sspcloudpro.co.in"
-TOKEN="fRv1Q16Kvq4krYItSosxzsATFk5dxjmc"
-CLUSTER_NAME="devops.sspcloudpro.co.in"
+API_SERVER="https://api.sspcloudpro.co.in"
+TOKEN="asbm9rjkHREcZED7d75oWv0U29bjdhn3"
+CLUSTER_NAME="sspcloudpro.co.in"
 
 kubectl config set preferences.colors true
 kubectl config set-cluster ${CLUSTER_NAME} --server=${API_SERVER} --insecure-skip-tls-verify=true
 kubectl config set-credentials ${CLUSTER_NAME} --token=${TOKEN}
-kubectl config set-context devops.sspcloudpro.co.in  --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=default
+kubectl config set-context sspcloudpro.co.in  --cluster=${CLUSTER_NAME} --user=${CLUSTER_NAME} --namespace=default
 # To test that configuration works
-kubectl get pods --context="devops.sspcloudpro.co.in" --namespace default
+kubectl get pods --context="sspcloudpro.co.in" --namespace default
 kubectl config get-contexts --namespace default
